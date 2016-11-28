3429e82e8ee37e219b57934f6790eeb12b84cb41
Sammy Jelin <sjelin@gmail.com>
2016-11-28 14:52:17 -0800

    chore(tests): e2e tests (#142)
    Travis only runs a subset of tests via `npm run test_travis`, but `npm test` runs a full suite.
    See https://github.com/angular/webdriver-manager/issues/165 for details on why travis cannot run
    all tests.

---
+++ e2e_spec/server_spec.ts
@@ -0,0 +1,28 @@
+import * as http from 'http';
+
+
+describe('sever smoke tests', () => {
+  it('should be able to ping selenium server', (done) => {
+    http.get('http://localhost:4444/selenium-server/driver/?cmd=getLogMessages', (resp) => {
+      expect(resp.statusCode).toBe(200);
+      let logs = '';
+      resp.on('data', (chunk) => logs += chunk);
+      resp.on('end', () => {
+        expect(logs).toContain('OK');
+        done()
+      });
+    });
+  });
+
+  it('should be able to ping appium server', (done) => {
+    http.get('http://localhost:4723/wd/hub/status', (resp) => {
+      expect(resp.statusCode).toBe(200);
+      let data = '';
+      resp.on('data', (chunk) => data += chunk);
+      resp.on('end', () => {
+        expect(JSON.parse(data).status).toBe(0);
+        done()
+      });
+    });
+  });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,37 @@
 'use strict';
 
+var path = require('path');
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var spawn = require('child_process').spawn;
 
-var runSpawn = function(done, task, opt_arg) {
-  opt_arg = typeof opt_arg !== 'undefined' ? opt_arg : [];
-  var child = spawn(task, opt_arg, {stdio: 'inherit'});
+var runSpawn = function(task, args, done) {
+  done = done || function() {};
+  var child = spawn(task, args, {stdio: 'inherit'});
   var running = false;
-  child.on('close', function() {
+  child.on('close', function(code) {
     if (!running) {
       running = true;
-      done();
+      done(code);
     }
   });
-  child.on('error', function() {
+  child.on('error', function(err) {
     if (!running) {
       console.error('gulp encountered a child error');
       running = true;
-      done();
+      done(err || 1);
     }
   });
+  return child;
 };
 
+// Build
 gulp.task('copy', function() {
   return gulp.src(['config.json', 'package.json'])
       .pipe(gulp.dest('built/'));
 });
 
-var tsGlobs = ['lib/**/*.ts', 'spec/**/*.ts'];
+var tsGlobs = ['lib/**/*.ts', '*spec/**/*.ts'];
 
 gulp.task('format:enforce', () => {
   const format = require('gulp-clang-format');
@@ -45,18 +48,57 @@ gulp.task('format', () => {
 });
 
 gulp.task('tsc', function(done) {
-  runSpawn(done, process.execPath, ['node_modules/typescript/bin/tsc']);
+  runSpawn(process.execPath, ['node_modules/typescript/bin/tsc'], done);
 });
 
 gulp.task('prepublish', function(done) {
   runSequence('tsc', 'copy', done);
 });
 
-gulp.task('default',['prepublish']);
-gulp.task('build',['prepublish']);
+gulp.task('default', ['prepublish']);
+gulp.task('build', ['prepublish']);
 
-gulp.task('test', ['format', 'build'], function(done) {
-  var opt_arg = [];
-  opt_arg.push('node_modules/jasmine/bin/jasmine.js');
-  runSpawn(done, process.execPath, opt_arg);
+// Command line commands
+gulp.task('update', ['build'], function(done) {
+  runSpawn(process.execPath, ['bin/webdriver-manager', 'update', '--android',
+      '--android-accept-licenses'], done);
 });
+gulp.task('start', ['build', 'shutdown'], function(done) {
+  runSpawn(process.execPath, ['bin/webdriver-manager', 'start', '--detach', '--seleniumPort',
+      '4444', '--android', '--appium-port', '4723', '--quiet'], done);
+});
+gulp.task('start:headless', ['build', 'shutdown'], function(done) {
+  runSpawn(process.execPath, ['bin/webdriver-manager', 'start', '--detach', '--seleniumPort',
+      '4444', '--android', '--appium-port', '4723', '--quiet', '--avds', 'none'], done);
+});
+gulp.task('shutdown', ['build'], function(done) {
+  runSpawn(process.execPath, ['bin/webdriver-manager', 'shutdown'], done);
+});
+
+// Test
+gulp.task('test:unit', ['build'], function(done) {
+  runSpawn(process.execPath, ['node_modules/jasmine/bin/jasmine.js'], done);
+});
+
+gulp.task('test:e2e:inner', ['build'], function(done) {
+  runSpawn(process.execPath, ['node_modules/jasmine/bin/jasmine.js', 'JASMINE_CONFIG_PATH=' +
+      path.join('e2e_spec', 'support', 'jasmine.json')], done);
+});
+gulp.task('test:e2e:inner:headless', ['build'], function(done) {
+  runSpawn(process.execPath, ['node_modules/jasmine/bin/jasmine.js', 'JASMINE_CONFIG_PATH=' +
+      path.join('e2e_spec', 'support', 'headless.json')], done);
+});
+gulp.task('test:e2e:no_update', function(done) {
+  runSequence('start', 'test:e2e:inner', 'shutdown', done);
+});
+gulp.task('test:e2e', function(done) {
+  runSequence('update', 'test:e2e:no_update', done);
+});
+gulp.task('test:e2e:headless', function(done) {
+  runSequence('update', 'start:headless', 'test:e2e:inner:headless', 'shutdown', done);
+});
+
+
+gulp.task('test', ['format', 'test:unit', 'test:e2e']);
+gulp.task('test:no_update', ['format', 'test:unit', 'test:e2e:no_update']);
+gulp.task('test:headless', ['format', 'test:unit', 'test:e2e:headless']);
