af6c41de4e88e9b48932e409226fdfb519cf41e2
Craig Nishina <craig.nishina@gmail.com>
2016-11-29 16:07:16 -0800

    revert experiment to parallelization

--- gulpfile.js
+++ gulpfile.js
@@ -99,9 +99,8 @@ gulp.task('test:e2e:headless', function(done) {
 });
 
 
-gulp.task('test', ['format', 'test:unit', 'test:e2e']);
-gulp.task('test:no_update', ['format', 'test:unit', 'test:e2e:no_update']);
-gulp.task('test:headless', ['format', 'test:unit', 'test:e2e:headless']);
-// gulp.task('test:headless', function(done) {
-//   runSequence('format', 'test:unit', 'test:e2e:headless', done);
-// });
+gulp.task('test', ['test:unit', 'test:e2e']);
+gulp.task('test:no_update', ['test:unit', 'test:e2e:no_update']);
+gulp.task('test:headless', function(done) {
+  runSequence('test:unit', 'test:e2e:headless', done);
+});
