6b483c97b7e2f027d3b26f4395c02e6798a34f52
Mike Perri <me@michaeljperri.com>
2017-03-23 13:21:35 -0400

    Change appiumPort to seleniumPort for selenium server request

--- lib/cmds/start.ts
+++ lib/cmds/start.ts
@@ -432,7 +432,7 @@ function signalWhenReady(
   }
   let pending = [waitFor(
       () => {
-        return request('GET', appiumPort, '/wd/hub/status', maxWait);
+        return request('GET', seleniumPort, '/wd/hub/status', maxWait);
       },
       (status) => {
         return JSON.parse(status).status == 0;
