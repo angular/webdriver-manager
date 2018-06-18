a2a0f1642bff8d8136470e6e236b0c8c524b5c64
Craig Nishina <craig.nishina@gmail.com>
2018-06-18 14:47:42 -0700

    chore(cleanup): clean up xml utils integration test

--- lib/update/downloader/xml_utils.spec-int.ts
+++ lib/update/downloader/xml_utils.spec-int.ts
@@ -1,9 +1,6 @@
 
 import * as child_process from 'child_process';
 import * as xml_utils from './xml_utils';
-import {httpServer} from '../../../spec/server/http_server';
-
-
 
 function spawnProcess(task: string, opt_arg?: string[], opt_io?: string) {
   opt_arg = typeof opt_arg !== 'undefined' ? opt_arg : [];
@@ -14,21 +11,15 @@ function spawnProcess(task: string, opt_arg?: string[], opt_io?: string) {
   return child_process.spawn(task, opt_arg, {stdio: stdio});
 }
 
-function killProcess(proc: any) {
-  proc.kill();
-}
-
 describe('xml_utils', () => {
   let proc: any;
   let origTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+
   beforeAll((done) => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
     proc = spawnProcess('npm', ['run', 'http-server']);
     console.log(proc.pid);
-    setTimeout(() => {
-      console.log('done!');
-      done();
-    }, 3000);
+    setTimeout(done, 3000);
   });
 
   it('should get the xml file', (done) => {
@@ -40,9 +31,6 @@ describe('xml_utils', () => {
   afterAll((done) => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = origTimeout;
     spawnProcess('kill', ['-TERM', proc.pid]);
-    setTimeout(() => {
-      console.log('done!');
-      done();
-    }, 3000);
+    setTimeout(done, 3000);
   });
 });
