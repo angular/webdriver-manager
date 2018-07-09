f149abdfed593a2e1d1b0c967c40aead0bbf62db
Craig Nishina <craig.nishina@gmail.com>
2018-07-09 15:20:05 -0700

    chore(lib): use index for lib and types

---
+++ lib/index.ts
@@ -0,0 +1,4 @@
+// Exports when using this module as a dependency.
+export { ChromeDriver } from './provider/chromedriver';
+export { GeckoDriver } from './provider/geckodriver';
+export { SeleniumServer } from './provider/selenium_server';
